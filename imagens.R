f1fac70310a228097c92041d4a0138eed0bb2efa
jtrecenti <julio.trecenti@gmail.com>
2015-02-08 13:04:46 -0200

    img1

--- imagens.R
+++ imagens.R
@@ -3,10 +3,8 @@ require(png)
 require(grid)
 
 ## plataforma
-p <- qplot(1:10, 1:10, geom="blank") +
-  annotation_raster(img, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf) +
-  theme_minimal()
-ggsave('img/portfolio/visualizacao.png', p, width=9, height=6.5, dpi=100)
+
+# IMG SEPARADA
 
 ## forecasting
 
